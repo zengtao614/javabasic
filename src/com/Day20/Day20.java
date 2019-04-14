dd4aaffafbb46db12539a92a68a8ac06c6c5d3a2
zengtao614 <zengtao614@gmail.com>
2019-04-14 15:35:16 +0800

    Day20学习IO流知识

--- src/com/Day20/Day20.java
+++ src/com/Day20/Day20.java
@@ -1,24 +1,61 @@
 package com.Day20;
 
-import java.io.File;
+import java.io.*;
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Date;
 
 public class Day20 {
     public static void main(String[] args) throws Exception{
 
-//        File file = new File("D:\\helloworld.java");
-//        System.out.println(file.getAbsolutePath());
-//        System.out.println(file.getPath());
-//        System.out.println(file.getParent());
-//        System.out.println(file.getName());
-//        System.out.println(file.getAbsoluteFile().getName());
-//        System.out.println(file.getCanonicalFile().getAbsolutePath());
+        //File类操作
+//        fileTest();
+
+
+        //文件字节输入流
+//        inputStreamTest();
+        //文件字节输出流
+        OutputStream os = new FileOutputStream("src/c.txt");
+        //ASCII码表
+        byte ASC[] = new byte[128];
+        for (int i=0;i<=127;i++){
+            ASC[i] = (byte)i;
+        }
+
+        //0:null
+
+        //48~57:0~9
+        os.write(ASC,48,10);
+        //32:spc(空格)
+        os.write(32);
+        //65~90:A~Z
+        os.write(ASC,65,26);
+        os.write(32);
+        //97~122:a~z
+        os.write(ASC,97,26);
+
+    }
+
+    private static void inputStreamTest() throws IOException {
+//        InputStream is = new FileInputStream(new File("src/c.txt"));
+//        while (is.read()!=-1)
+//        byte b[] = new byte[10];
+//        System.out.println(is.read());
+//        System.out.println(Arrays.toString(b));
+    }
+
+    private static void fileTest() throws IOException {
+        File file = new File("D:\\helloworld.java");
+        System.out.println(file.getAbsolutePath());
+        System.out.println(file.getPath());
+        System.out.println(file.getParent());
+        System.out.println(file.getName());
+        System.out.println(file.getAbsoluteFile().getName());
+        System.out.println(file.getCanonicalFile().getAbsolutePath());
 
         File file1 = new File("src/com/Day20/a.txt");
         System.out.println(file1.createNewFile());
         //重命名加剪切粘贴过程
         System.out.println(file1.renameTo(new File("src/b.txt")));
-
     }
 }
---
+++ src/com/Day20/Test.java
@@ -0,0 +1,52 @@
+package com.Day20;
+
+import java.io.*;
+
+public class Test {
+    public static void main(String[] args) throws Exception{
+
+        //File类操作
+        fileTest();
+        //字节输入流
+        byte[] b = inputStreamTest();
+        //字节输出流
+        outputStreamTest(b);
+        //边读边写
+        inputAndoutputTest();
+
+
+    }
+
+    private static void inputAndoutputTest() throws IOException {
+        InputStream is = new FileInputStream("src\\c.txt");
+        OutputStream os = new FileOutputStream("src\\d.txt");
+        int i;
+        while ((i=is.read())!=-1){
+            os.write(i);
+        }
+    }
+
+    private static void outputStreamTest(byte[] b) throws IOException {
+        OutputStream os = new FileOutputStream("src\\d.txt");
+        os.write(b);
+    }
+
+    private static byte[] inputStreamTest() throws IOException {
+        InputStream is = new FileInputStream("src\\b.txt");
+        System.out.println(is.read());
+        byte b[] = new byte[10];
+        System.out.println(is.read(b));
+        System.out.println(is.read(b,0,6));
+        return b;
+    }
+
+    private static void fileTest() {
+        File file = new File("src\\c.txt");
+        System.out.println(file.exists());
+        System.out.println(file.length());
+        System.out.println(file.getName());
+        System.out.println(file.getAbsolutePath());
+        System.out.println(file.getPath());
+        System.out.println(file.isFile());
+    }
+}
